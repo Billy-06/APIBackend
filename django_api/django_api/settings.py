618178294495af5d39d51a57b88cd1ce0b07cbe5
Billy-06 <ndegwah4billy@gmail.com>
2024-02-29 18:49:06 +0800

    Updated CORS while_list- removed trailing slash

--- django_api/django_api/settings.py
+++ django_api/django_api/settings.py
@@ -55,6 +55,24 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
+
+CORS_ALLOW_HEADERS = (
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+)
+
 CORS_ORIGIN_WHITELIST = [
     # Preferred Origins here....
     "http://127.0.0.1:3000",
